34d7d68e4b1d042e4e15cac5c63d27f21fac0bd4
Divine <divine.tsmaredi@gmail.com>
2024-08-06 09:33:14 +0200

    gif commit

--- preloader.js
+++ preloader.js
@@ -16,7 +16,7 @@ overlay.style.cssText = `
 
 const svgImage = document.createElement('img');
 svgImage.id = 'svgImage';
-svgImage.src = 'https://cdn.jsdelivr.net/gh/divinemaredi/flutter_preloader-main/animation.json';
+svgImage.src = 'https://cdn.jsdelivr.net/gh/divinemaredi/flutter_preloader-main/animationtest.gif';
 svgImage.style.cssText = `
     max-width: 100%;
     max-height: 100%;
